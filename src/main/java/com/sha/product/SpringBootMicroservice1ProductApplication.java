9604d3a1512ed28b01b10d567ddb2330589170d1
ZuhalCetin23 <zuhalcetin@ogm.gov.tr>
2022-02-03 13:39:11 +0300

    initial commit

--- src/main/java/com/sha/product/SpringBootMicroservice1ProductApplication.java
+++ src/main/java/com/sha/product/SpringBootMicroservice1ProductApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication (exclude = SecurityAutoConfiguration.class)
-@PropertySource("classpath:application-${spring.profiles.active.default}.properties")
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroservice1ProductApplication {
 
 	public static void main(String[] args) {
